5f8ff6e18921c32f7348ea85bed80192d038a8c3
Harrison <jiumingwang@cmi.chinamobile.com>
2025-04-02 16:07:05 +0800

    update http setting

--- start.sh
+++ start.sh
@@ -2,11 +2,24 @@
 
 # 启动前端服务
 cd /app/frontend/dist
-nohup python -m http.server 8080 &
+# 使用更好的静态文件服务器
+python3 -c '
+import http.server
+import socketserver
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if not "." in self.path:
+            self.path = "index.html"
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+with socketserver.TCPServer(("", 8080), Handler) as httpd:
+    print("前端服务已启动在端口 8080")
+    httpd.serve_forever()
+' &
 
 # 等待前端服务启动
 sleep 2
-echo "前端服务已启动在端口 8080"
 
 # 启动后端服务
 cd /app/backend
